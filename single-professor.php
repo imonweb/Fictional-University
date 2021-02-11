aa83e1196d6f646541523b13dce74ba27f0e6f26
Imon <contact@imonweb.co.uk>
2021-02-11 14:26:00 +0000

    post thumbnail

--- single-professor.php
+++ single-professor.php
@@ -15,8 +15,16 @@
     </div>
 
     <div class="container container--narrow page-section">
-
-      <div class="generic-content"><?php the_content(); ?></div>
+    <?php //the_post_thumbnail(); the_content(); ?>
+      <div class="generic-content"></div>
+        <div class="row group">
+          <div class="one-third">
+            <?php the_post_thumbnail(); ?>
+          </div>
+          <div class="two-thirds">
+            <?php the_content(); ?>
+          </div>
+        </div>
       
       <?php 
           $relatedPrograms = get_field('related_programs');
