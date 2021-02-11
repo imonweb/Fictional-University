861f13e9808cbb89d591aa508c514325476ab02c
Imon <contact@imonweb.co.uk>
2021-02-11 22:49:44 +0000

    Page Banner Functions

--- functions.php
+++ functions.php
@@ -9,8 +9,16 @@ function pageBanner($args = NULL){
     $args['subtitle'] = get_field('page_banner_subtitle');
   }
 
-  if(!$args['photo']) {
-    if(get_field('page_banner_background_image')){
+  // if(!$args['photo']) {
+  //   if(get_field('page_banner_background_image')){
+  //     $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+  //   } else {
+  //     $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+  //   }
+  // }
+
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
       $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
     } else {
       $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
