5915f6247a3506a9ccfbf6e38f34ebb6d0f394c2
Imon <contact@imonweb.co.uk>
2021-02-15 21:24:50 +0000

    Asyncronous added

--- functions.php
+++ functions.php
@@ -49,8 +49,8 @@ function university_files() {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendor.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.ee34691ca7a08d20fc09.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.ee34691ca7a08d20fc09.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.24de78e7df374821e6ea.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.24de78e7df374821e6ea.css'));
   }
 
   wp_localize_script('main-university-js','universityData',array(
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 class Search {
   // 1. describe and create/initiate our object
   constructor() {
+    // this.addSearchHTML();
     this.resultsDiv = $("#search-overlay__results");
     this.openButton =  $(".js-search-trigger");
     this.closeButton = $(".search-overlay__close");
@@ -36,7 +37,7 @@ class Search {
             this.resultsDiv.html('<div class="spinner-loader"></div>');
             this.isSpinnerVisible = true;
           }
-        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750);
         } else {
           this.resultsDiv.html('');
           this.isSpinnerVisible = false;
@@ -53,19 +54,44 @@ class Search {
     // before testArray.map
     // <li><a href="${posts[0].link}">${posts[0].title.rendered}</a></li>
     // 'http://localhost/wp/Fictional-University/wp-json/wp/v2/posts?search='
+  
     
-    $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
       // alert(posts[0].title.rendered);
       // var testArray = ['red', 'orange', 'yellow'];
-      this.resultsDiv.html(`
+
+      //ASYNCHRONOUS
+      $.when(
+        $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
+        $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
+        ).then((posts, pages) => {
+        var combinedResults = posts[0].concat(pages[0]);
+        this.resultsDiv.html(`
         <h2 class="search-overlay__section-title">General Information</h2>
-        ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search'}
-          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-        ${posts.length ? '</ul>' : ''}
+        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search'}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+        ${combinedResults.length ? '</ul>' : ''}
       `);
       this.isSpinnerVisible = false;
+      }, () => {
+        this.resulsDiv.html('<p>Unexpected error; please try again.</p>');
+      });
+
+/*    
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(),pages => {
+        var combinedResults = posts.concat(pages);
+        this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search'}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+        ${combinedResults.length ? '</ul>' : ''}
+      `);
+      this.isSpinnerVisible = false;
+      });
        // ${testArray.map(item => `<li>${item}</li>`).join('')}
     });
+  */
+     
   }
 
   keyPressDispatcher(e) {
@@ -84,6 +110,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
     $("body").addClass("body-no-scroll");
+    this.searchField.val('');
+    setTimeout(()=> this.searchField.focus(), 301);
     console.log("our open method just ran!");
     this.isOverlyOpen = true;
   }
@@ -93,6 +121,26 @@ class Search {
     console.log("our close method just ran!");
     this.isOverlayOpen = true;
   }
-}
+
+  addSearchHTML(){
+    document.body.innerHTML += `
+      <div class="search-overlay">
+      <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="what are you looking for?" id="search-term" autocomplete="off">
+          </div>
+      </div>
+      <div class="container">
+        <div id="search-overlay__results">
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+
+} // end Search class
 
 export default Search;
