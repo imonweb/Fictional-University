b5761d59955a2223a0019b4e3802c85595ac903c
Imon <contact@imonweb.co.uk>
2021-02-16 14:31:55 +0000

    Column Layout for Search Overlay

--- functions.php
+++ functions.php
@@ -58,8 +58,8 @@ function university_files() {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendor.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.d83c38d95d5ca68bd42a.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.d83c38d95d5ca68bd42a.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.cbd45d8be44ff9ffec89.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.cbd45d8be44ff9ffec89.css'));
   }
 
   wp_localize_script('main-university-js','universityData',array(
