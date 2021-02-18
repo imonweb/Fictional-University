9f97377245ff9adc8f198761d9d6bd57be93fbd2
Imon <contact@imonweb.co.uk>
2021-02-18 17:20:21 +0000

    Registration and backend login form added

--- functions.php
+++ functions.php
@@ -149,3 +149,23 @@ function noSubsAdminBar() {
      show_admin_bar(false);
   }
 }
+
+/*  Customize Login Screen */
+add_filter('login_headerurl','ourHeaderUrl');
+
+function ourHeaderUrl(){
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS(){
+  wp_enqueue_style('custom-google-fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.9911c4657b19efe95a5e.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle(){
+  return get_bloginfo('name');
+}
