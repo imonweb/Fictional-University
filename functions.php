71853825217167b9f72946d2559978b407cbf5a0
Imon <contact@imonweb.co.uk>
2021-02-20 01:59:03 +0000

    note single post delete module

--- functions.php
+++ functions.php
@@ -58,12 +58,13 @@ function university_files() {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendor.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.16cf01c99c0eb78b52e0.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.16cf01c99c0eb78b52e0.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.fa7ca4ca1cc46943aab9.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.fa7ca4ca1cc46943aab9.css'));
   }
 
   wp_localize_script('main-university-js','universityData',array(
-    'root_url' => get_site_url()
+    'root_url' => get_site_url(),
+    'nonce' => wp_create_nonce('wp_rest')
   ));
   
 }
@@ -161,7 +162,7 @@ add_action('login_enqueue_scripts', 'ourLoginCSS');
 
 function ourLoginCSS(){
   wp_enqueue_style('custom-google-fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.16cf01c99c0eb78b52e0.css'));
+  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.fa7ca4ca1cc46943aab9.css'));
 }
 
 add_filter('login_headertitle', 'ourLoginTitle');
--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -11,7 +11,26 @@ class MyNotes {
 
   // Methods will go here
   deleteNote(){
-    console.log('delete');
+    // console.log('delete');
+    // var thisNote = $(e.target).parents("li");
+
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      // url: universityData.root_url + '/wp-json/wp/v2/note/109',
+      url: universityData.root_url + '/wp-json/wp/v2/note/109',
+      type: 'DELETE',
+      success: (response) => {
+        console.log("Congrats");
+        console.log(response);
+      },
+      error: (response) => {
+
+        console.log("Sorry");
+        console.log(response);
+      }
+    });
   }
  }
 
