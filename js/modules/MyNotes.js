550617db8d67d2e5b420f43c219c02bd54f8158c
Imon <contact@imonweb.co.uk>
2021-02-20 11:10:00 +0000

    MyNotes Add Function added

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -6,19 +6,46 @@ class MyNotes {
   }
 
   events(){
-    $(".delete-note").on("click", this.deleteNote)
+    $(".delete-note").on("click", this.deleteNote);
+    $(".edit-note").on("click", this.editNote);
   }
 
   // Methods will go here
-  deleteNote(){
-    // console.log('delete');
-    // var thisNote = $(e.target).parents("li");
+  editNote(e){
+    var thisNote = $(e.target).parents("li");
+    thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+  } // edit Note
+
+  deleteNote(e){
+    var thisNote = $(e.target).parents("li");
 
     $.ajax({
       beforeSend: (xhr) => {
         xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
       },
-      // url: universityData.root_url + '/wp-json/wp/v2/note/109',
+      url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+      type: 'DELETE',
+      success: (response) => {
+        thisNote.slideUp();
+        console.log("Congrats");
+        console.log(response);
+      },
+      error: (response) => {
+
+        console.log("Sorry");
+        console.log(response);
+      }
+    });
+  }
+
+  /*  Delete Function single id test */
+  /*
+  deleteNote(){
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+  
       url: universityData.root_url + '/wp-json/wp/v2/note/109',
       type: 'DELETE',
       success: (response) => {
@@ -32,6 +59,7 @@ class MyNotes {
       }
     });
   }
- }
+  */
+ } // end MyNotes class
 
  export default MyNotes;
