21990bb3dfb8b30528bcaa30ac314d1337a0382c
Imon <contact@imonweb.co.uk>
2021-02-20 22:15:36 +0000

    Create New Note

--- functions.php
+++ functions.php
@@ -58,8 +58,8 @@ function university_files() {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendor.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.9addf0286f7d9f2310c4.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.9addf0286f7d9f2310c4.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.c496047a4794634eba85.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.c496047a4794634eba85.css'));
   }
 
   wp_localize_script('main-university-js','universityData',array(
@@ -162,7 +162,7 @@ add_action('login_enqueue_scripts', 'ourLoginCSS');
 
 function ourLoginCSS(){
   wp_enqueue_style('custom-google-fonts','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.9addf0286f7d9f2310c4.css'));
+  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.c496047a4794634eba85.css'));
 }
 
 add_filter('login_headertitle', 'ourLoginTitle');
